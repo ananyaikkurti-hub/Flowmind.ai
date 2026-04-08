5a6e6bbc18fa6d486c5582b12d916f38ad20757a
Ananya Ikkurti <ananyaikkurti@gmail.com>
2026-04-09 00:19:58 +0530

    fix: flash

--- lib/gemini.ts
+++ lib/gemini.ts
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI(
 
 export async function generateAI(prompt: string) {
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.0-flash",,
   });
 
   const result = await model.generateContent(prompt);
