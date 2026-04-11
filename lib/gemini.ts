f6bc45076ae038e40a18249fa4b45baffa26189e
Ananya Ikkurti <ananyaikkurti@gmail.com>
2026-04-11 18:55:06 +0530

    fix: , new 1 API Key

--- lib/gemini.ts
+++ lib/gemini.ts
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI(
 
 export async function generateAI(prompt: string) {
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash",
+    model: "gemini-1.5-flash-latest",
   });
 
   const result = await model.generateContent(prompt);
